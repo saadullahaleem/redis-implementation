912f045e60ea403bb735b35a1ddc60575d685295
Saad <aleemsaadullah@gmail.com>
2024-10-14 19:00:05 -0400

    --allow-empty

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
     client, addr = server_socket.accept()
     client.send(b"+PONG\r\n")
 
